76d52ab999269f93d9c3b30c34327ab41894fd18
Vera Ciocan <guess13grl@gmail.com>
2023-11-28 11:38:00 +0100

    Done users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attribute :is_doctor, default: false
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_100253) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_103246) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_100253) do
     t.string "first_name"
     t.string "last_name"
     t.integer "postal_code"
+    t.boolean "is_doctor"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
