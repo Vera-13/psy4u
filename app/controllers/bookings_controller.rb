3008be85332f4319bd452271d9dc2599de95e311
Vera Ciocan <guess13grl@gmail.com>
2023-11-28 17:05:46 +0100

    Worked on bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,10 +5,19 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking.doctor = User.find(params?)
+    @booking = Booking.new(booking_params)
+    @booking.doctor = User.find(params[:doctor_id])
+    if @booking.save
+      redirect_to doctor_path(@doctor)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to doctor_path(@bookmark.doctor), status: :see_other
   end
 
   private
